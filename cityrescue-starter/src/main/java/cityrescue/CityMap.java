c5d9db6f6acf4e42b7e9f1f5d33781af677527fb
WilliamMK07 <wmk208@exeter.ac.uk>
2026-02-19 20:17:04 +0000

    city map Co-authored-by: msnshorenye <msnshorenye@users.noreply.github.com>

---
+++ cityrescue-starter/src/main/java/cityrescue/CityMap.java
@@ -0,0 +1,14 @@
+package cityrescue;
+
+public class CityMap {
+    public int width;
+    public int height;
+
+    public(int new_width, int new_height){
+     width = new_width;
+     height = this.height;
+    }
+    public static void main(Stringargs[])
+
+
+}
